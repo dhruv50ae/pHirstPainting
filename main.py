c39561cb9f7b95a373ffcd38f397df8c447c7593
dhruv50ae <linkdp2012@gmail.com>
2022-07-10 07:56:21 +0100

    add files

---
+++ main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+rammus = Turtle()
+
+rammus.shape("turtle")
+rammus.color("purple")
+rammus.forward(100)
+rammus.right(90)
+rammus.forward(100)
+rammus.right(90)
+rammus.forward(100)
+rammus.right(90)
+rammus.forward(100)
+rammus.right(90+45)
+rammus.forward(146)
+
+screen = Screen()
+screen.exitonclick()
+
