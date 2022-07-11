0bffdd65bfa8f0a04cfab76bf1cecc2f3dfe81f4
dhruv50ae <linkdp2012@gmail.com>
2022-07-11 19:06:41 +0100

    Finish final challenge

--- main.py
+++ main.py
@@ -2,25 +2,15 @@ from turtle import Turtle, Screen
 import random
 
 rammus = Turtle()
-
-rammus.colormode(255)
-
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    random_colour = (r, g, b)
-    return random_colour
-
 directions = [0, 90, 180, 270]
-
-rammus.pensize(15)
 rammus.speed('fastest')
 
-for _ in range(200):
-    rammus.color(random_color())
-    rammus.forward(30)
-    rammus.setheading(random.choice(directions))
+def drawSpiro(sizeOfGap):
+    for _ in range(int(360/sizeOfGap)):
+        rammus.circle(100)
+        rammus.setheading(rammus.heading()+sizeOfGap)
+
+drawSpiro(5)
 
 screen = Screen()
 screen.exitonclick()
