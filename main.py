7c3d293b52e1f4a7c9191d2dfbc477dd0adfd27d
dhruv50ae <linkdp2012@gmail.com>
2022-07-12 22:52:09 +0100

    Sort basic project

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from turtle import Turtle, Screen
-import random
+import colorgram
 
-rammus = Turtle()
-directions = [0, 90, 180, 270]
-rammus.speed('fastest')
-
-def drawSpiro(sizeOfGap):
-    for _ in range(int(360/sizeOfGap)):
-        rammus.circle(100)
-        rammus.setheading(rammus.heading()+sizeOfGap)
-
-drawSpiro(5)
-
-screen = Screen()
-screen.exitonclick()
+# colours = colorgram.extract('975994.jpg', 30)
+# rgbColours = []
+# for colour in colours:
+#     r = colour.rgb.r
+#     g = colour.rgb.g
+#     b = colour.rgb.b
+#     newColour = (r, g, b)
+#     rgbColours.append(newColour)
+# print(rgbColours)
+colourList = [(25, 15, 12), (220, 229, 238), (91, 93, 98), (46, 41, 43), (153, 160, 170), (254, 254, 254), (50, 50, 53), (82, 78, 76), (82, 78, 80), (248, 254, 253), (182, 191, 202), (62, 62, 67), (122, 127, 135), (67, 61, 64), (251, 249, 251), (82, 85, 84), (72, 60, 57), (34, 37, 35), (156, 151, 147), (152, 145, 149), (66, 64, 59), (150, 155, 153), (133, 124, 129), (114, 132, 138), (53, 67, 71), (140, 122, 119), (174, 197, 206), (60, 66, 60), (131, 128, 121), (202, 185, 189)]
 
